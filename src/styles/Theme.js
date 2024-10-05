bfa2d0140731466ce9299137cc9c9ce54ae5d2b1
youminki <dbalsrl7648@gmail.com>
2024-10-05 12:51:35 +0900

    Setting : GlobalStyle 폼 설정

---
+++ src/styles/Theme.js
@@ -0,0 +1,56 @@
+import { css } from 'styled-components';
+
+export const Theme = {
+  fonts: {
+    mainTitle: css`
+      font-family: 'Noto Sans KR', sans-serif;
+      font-weight: 400;
+      font-size: 54px;
+    `,
+    heading: css`
+      font-family: 'Noto Sans KR', sans-serif;
+      font-weight: 400;
+      font-size: 28px;
+    `,
+    default: css`
+      font-family: 'Noto Sans KR', sans-serif;
+      font-weight: 400;
+      font-size: 20px;
+    `,
+    helperText: css`
+      font-family: 'Noto Sans KR', sans-serif;
+      font-weight: 400;
+      font-size: 16px;
+    `,
+    BigButton: css`
+      font-family: 'Noto Sans KR', sans-serif;
+      font-weight: 600;
+      font-size: 30px;
+    `,
+    SmallButton: css`
+      font-family: 'Noto Sans KR', sans-serif;
+      font-weight: 800;
+      font-size: 20px;
+    `,
+    subTitle: css`
+      font-family: 'Noto Sans KR', sans-serif;
+      font-weight: 600;
+      font-size: 36px;
+      line-height: 44px;
+    `,
+  },
+  colors: {
+    green1: '#7fa67e',
+    green2: '#8bb38a',
+    green3: '#97bf96',
+    green4: '#a3cca2',
+    green5: '#afd9ae',
+    green6: '#bce6bb',
+    green7: '#c9f3c7',
+  },
+  breakpoints: {
+    mobile: '480px',
+    tablet: '769px',
+    desktop: '1024px',
+  },
+};
