543074fc044d5bfdee46bf2c6fb965dbc6108d21
youminki <dbalsrl7648@gmail.com>
2024-10-06 04:02:57 +0900

    fix : 쿠키 재설정

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+// Axios 인스턴스 생성
 export const Axios = axios.create({
   baseURL: 'http://3.39.195.75:8080',
-  withCredentials: true,
+  withCredentials: true, // 서버가 쿠키를 보내도록 설정
 });
 
+// 응답 인터셉터 설정
 Axios.interceptors.response.use(
   (response) => {
     return response;
@@ -13,7 +15,25 @@ Axios.interceptors.response.use(
     if (window.location.pathname === '/login') {
       return Promise.reject(error);
     }
+    return Promise.reject(error);
+  }
+);
 
+// 요청 인터셉터에서 쿠키 추가
+Axios.interceptors.request.use(
+  (config) => {
+    const token = document.cookie // 또는 localStorage에서 쿠키나 토큰을 가져옴
+      .split('; ')
+      .find((row) => row.startsWith('token='))
+      ?.split('=')[1];
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`; // 토큰이 있을 경우 헤더에 추가
+    }
+
+    return config;
+  },
+  (error) => {
     return Promise.reject(error);
   }
 );
